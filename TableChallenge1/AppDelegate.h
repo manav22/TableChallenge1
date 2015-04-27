cd641c4792efc1783ae120da1c3fc0d60d00e726
Manav Pavitra Singh <manavpavitrasingh@Manavs-MacBook-Pro.local>
2015-04-27 15:47:18 -0700

    Initial Commit

---
+++ TableChallenge1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableChallenge1
+//
+//  Created by Manav Pavitra Singh on 4/27/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableChallenge1Tests/TableChallenge1Tests.m
@@ -0,0 +1,40 @@
+//
+//  TableChallenge1Tests.m
+//  TableChallenge1Tests
+//
+//  Created by Manav Pavitra Singh on 4/27/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TableChallenge1Tests : XCTestCase
+
+@end
+
+@implementation TableChallenge1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
